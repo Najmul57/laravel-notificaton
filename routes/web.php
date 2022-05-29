a56d6f34355b10561e0a9f3da414181c6d6135b6
najmul <najmulcse2@gmail.com>
2022-05-29 12:55:45 +0600

    send email notification using notifiy and facade

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Notifications\EmailNotification;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Notification;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('notificatioin',function(){
+    $user =User::find(1);
+    // $user->notify(new EmailNotification());
+    Notification::send($user, new EmailNotification());
+    return redirect()->back();
+});
 
 Route::get('/', function () {
     return view('welcome');
